919d3e808f6aecf68144ccf487ae4e67e9ff494c
Atroopa <atroopa@gmail.com>
2023-01-24 19:05:53 +0330

    work on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useState } from 'react';
 import {FaBar, FaTimes} from 'react-icons/fa';
-
+import Logo from '../assets/logo.png';
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+    const handleClick   = () => setNav(!nav);
+
   return (
-    <div>Navbar</div>
+    <div className=' fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-white'>
+        <div >
+            <img src={Logo} alt='logo' className='w-[50px]' />
+        </div>
+
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+    </div>
   );
 }
 
