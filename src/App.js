a95f91be91be76ef7c5137e6e100e789a55e3b81
Atroopa <atroopa@gmail.com>
2023-01-24 16:46:02 +0330

    create navbar file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className='App bg-red-100 text-3xl text-black'>
-      using bootstrap
+    <div>
+      
     </div>
   );
 }
