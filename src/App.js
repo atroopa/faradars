040127d51a45a649a36dda37d3ea9a98a91d9016
Atroopa <atroopa@gmail.com>
2023-01-24 16:44:54 +0330

    tailwind css test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App bg-dark text-white'>
+    <div className='App bg-red-100 text-3xl text-black'>
       using bootstrap
     </div>
   );
