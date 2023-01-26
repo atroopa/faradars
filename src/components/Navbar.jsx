cac2b1da7862d143e1422922f6c20a2ae540050b
Atroopa <atroopa@gmail.com>
2023-01-26 17:02:01 +0330

    navbar step one finish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import {FaBar, FaTimes} from 'react-icons/fa';
+import { FaBars, FaTimes} from 'react-icons/fa';
 import Logo from '../assets/logo.png';
 
 const Navbar = () => {
@@ -14,13 +14,37 @@ const Navbar = () => {
             <img src={Logo} alt='logo' className='w-[50px]' />
         </div>
 
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+        <ul className='hidden md:flex'>
+            <li>تماس</li>
+            <li>پروژه ها</li>
+            <li>مهارت ها</li>
+            <li>درباه</li>
+            <li>خانه</li>
         </ul>
+
+
+        <div onClick={handleClick} className='md:hidden z-10 cursor-pointer'>
+            {
+                !nav ? <FaBars/> : <FaTimes/>
+            }
+        </div>
+
+
+        <div
+        className={
+            !nav
+                ? "hidden"
+                : "absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center"
+            }
+        >
+            <ul>
+                <li className='py-6 text-4xl'>تماس</li>
+                <li className='py-6 text-4xl'>پروژه ها</li>
+                <li className='py-6 text-4xl'>مهارت ها</li>
+                <li className='py-6 text-4xl'>درباه</li>
+                <li className='py-6 text-4xl'>خانه</li>
+            </ul>
+        </div>
 
     </div>
   );
