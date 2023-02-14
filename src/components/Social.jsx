8b5152ddc7f1a0d7ded348fcd46ed41083a2b033
Atroopa <atroopa@gmail.com>
2023-02-14 18:45:19 +0330

    add code to social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Social = () => {
   return (
     <div>
-      Social
+        <div className="flex items-center">
+    <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-linkedin fa-lg mx-4"></i>
+    </a>
+    <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-github fa-lg mx-4"></i>
+    </a>
+    <a href="https://twitter.com/your-username" target="_blank" rel="noopener noreferrer">
+      <i className="fab fa-twitter fa-lg mx-4"></i>
+    </a>
+  </div>
     </div>
   );
 }
